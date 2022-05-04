4086c914fc72207b46b31c2a8a1eb07c2c5449ad
Alejandra Perdomo <alejandraperdomo605@yahoo.com>
2022-05-04 16:32:54 -0600

    Modified app.js to deploy on heroku.

--- API-Master/app.js
+++ API-Master/app.js
@@ -21,4 +21,6 @@ app.get('/tweets',(req,res)=>{
     })
 })
 
-app.listen(3000,()=>{console.log("Listening on Twitter's port 3000")});
+const port=process.env.PORT || 3000
+
+app.listen(port,()=>{console.log("Listening on Twitter's port 3000")});
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: node API-Master/app.js
