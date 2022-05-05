53edf1016df84c231dc47a5bc1c2b5f590190a4b
Alejandra Perdomo <alejandraperdomo605@yahoo.com>
2022-05-05 11:55:36 -0600

    Deleted subdirectories

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 const express=require('express');
 const app=express();
-const Twitter=require('./API/helpers/twitter');
+const Twitter=require('./twitter');
 const tweets=new Twitter();
 require('dotenv').config();
 
